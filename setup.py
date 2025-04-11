7bb3d90ac2b496947c3cc7a78282e45c2c8161bf
dctalbot <David.Talbot@superconductive.com>
2025-04-11 12:46:31 -0400

    configuration

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ history = open('CHANGES.rst').read().replace('.. :changelog:', '')
 setup(
     name='gx-sqlalchemy-redshift',
     version='0.8.15.dev0',
-    description='Forked Amazon Redshift Dialect for sqlalchemy',
+    description='Great Expectations fork of the Amazon Redshift sqlalchemy dialect',
     long_description=history,
     long_description_content_type='text/x-rst',
     author='The Great Expectations Team',
@@ -14,7 +14,7 @@ setup(
     url='https://github.com/great-expectations/sqlalchemy-redshift',
     packages=['sqlalchemy_redshift', 'redshift_sqlalchemy'],
     package_data={'sqlalchemy_redshift': ['redshift-ca-bundle.crt']},
-    python_requires='>=3.4',
+    python_requires='>=3.9',
     install_requires=[
         'SQLAlchemy>2.0.7',
         'packaging',
@@ -27,20 +27,17 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     entry_points={
         'sqlalchemy.dialects': [
             'redshift = sqlalchemy_redshift.dialect:RedshiftDialect_psycopg2',
             'redshift.psycopg2 = sqlalchemy_redshift.dialect:RedshiftDialect_psycopg2',
-            'redshift.psycopg2cffi = sqlalchemy_redshift.dialect:RedshiftDialect_psycopg2cffi',
-            'redshift.redshift_connector = sqlalchemy_redshift.dialect:RedshiftDialect_redshift_connector',
+            # 'redshift.psycopg2cffi = sqlalchemy_redshift.dialect:RedshiftDialect_psycopg2cffi',
+            # 'redshift.redshift_connector = sqlalchemy_redshift.dialect:RedshiftDialect_redshift_connector',
         ]
     },
 )
