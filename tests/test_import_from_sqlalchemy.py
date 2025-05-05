43c3173ac594703a0f2ad14b7efd86c384a35f72
dctalbot <David.Talbot@superconductive.com>
2025-05-05 14:36:36 -0400

    WIP

--- tests/test_import_from_sqlalchemy.py
+++ tests/test_import_from_sqlalchemy.py
@@ -1,2 +1,7 @@
+from sqlalchemy import create_engine
+
+
 def test_stub():
-    assert 1 == 1
+    create_engine(
+        "redshift+psycopg2://username:password@my-redshift-cluster.abcdef123456.us-west-2.redshift.amazonaws.com:5439/mydatabase"
+    )
